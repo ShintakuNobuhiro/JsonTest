6a7b38ecc9e49ee8f7eaee96f9681c930e1f5dc0
Shintaku <ukyankyan11@gmail.com>
2015-09-18 10:25:10 +0900

    Commit

--- app/src/main/java/com/example/shintaku/myapplication/TestActivity.java
+++ app/src/main/java/com/example/shintaku/myapplication/TestActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TestActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
@@ -25,6 +29,19 @@ public class TestActivity extends Activity {
                 Log.d("onPostExecute", result);
                 final Button btn = (Button) findViewById(R.id.button);
                 final TextView textView = (TextView) findViewById(R.id.textView);
+                try {
+                    JSONObject json = new JSONObject(result);
+                    JSONArray missions = json.getJSONArray("missions");
+                    String description = null;
+                    for (int i = 0; i < missions.length(); i++) {
+                        JSONObject mission = missions.getJSONObject(i);
+                        String category = mission.getString("category");
+                        description = mission.getString("description");
+                        String exp = mission.getString("exp");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
